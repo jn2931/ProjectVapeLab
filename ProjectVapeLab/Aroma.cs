b7e2b1e53c5e0c1a603d58ad784c3a47b4ec3c4a
Jakub <jakubnowicki2931@poczta.onet.pl>
2021-05-23 02:01:11 +0200

    Added aroma and eliquid class prototype.

---
+++ ProjectVapeLab/Aroma.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectVapeLab
+{
+    class Aroma
+    {
+        public string name;
+        private int percentage;
+        public Aroma(string aromaName,int aromaPercentage)
+        {
+            name = aromaName;
+            percentage = aromaPercentage;
+        }
+    }
+}
